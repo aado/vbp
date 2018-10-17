3673c4eea96a55aba8bc947e692c28713a8620bd
alvin <alvin@outsourceglobal.com>
2018-10-17 16:16:29 +0800

    test chat

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Pusher from 'pusher-js';
+import ChatList from './ChatList';
+import ChatBox from './ChatBox';
 
 class App extends Component {
   constructor(props) {
