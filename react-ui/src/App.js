e46ad78485c5d6bc6d788cf7e03811b9e997c97f
alvin <alvin@outsourceglobal.com>
2018-10-18 12:33:37 +0800

    master

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
 	constructor(props) {
@@ -35,6 +36,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+		<Router>
+			<div>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/authexample" component={Authexample} />
+				<Route exact path="/tasktemplate" component={TaskTemplate} />
+				<Route exact path="/clients" component={Clients} />
+				<Route exact path="/users" component={Users} />
+			</div>
+		</Router>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to VBP</h2>
