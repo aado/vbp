864ee03986b59911ae68f0c5c113df9f847e2db6
alvin <alvin@outsourceglobal.com>
2018-10-19 17:13:30 +0800

    master

--- react-ui/src/pages/Users.js
+++ react-ui/src/pages/Users.js
@@ -170,7 +170,7 @@ export default class Users extends Component {
 	addNewUser(addUser) {
 		axios.request({
 			method:'post',
-			url:'https://e8683824.ngrok.io/vbp/api/vbpapi/users/adduser',
+			url:'http://localhost:8080/vbp/api/vbpapi/users/adduser',
 			data: addUser
 		}).then(response => {
 			this.setState({open: false, modal: false});
