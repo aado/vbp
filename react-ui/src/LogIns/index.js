adf47c49771be445ddadc818cb2f1f193886d27c
alvin <alvin@outsourceglobal.com>
2018-10-31 15:56:15 +0800

    changes

--- react-ui/src/LogIns/index.js
+++ react-ui/src/LogIns/index.js
@@ -5,23 +5,23 @@ import '../components/signin.css'
 import app from "../base";
 
 class LogInContainer extends Component {
-  handleSignUp = async event => {
-    event.preventDefault();
-    const { email, password } = event.target.elements;
-    try {
-      const user = await app
-        .auth()
-        .signInWithEmailAndPassword(email.value, password.value);
-      this.props.history.push("/");
-      console.log(user);
-    } catch (error) {
-      alert(error);
-    }
-  };
+	handleSignUp = async event => {
+		event.preventDefault();
+		const { email, password } = event.target.elements;
+		try {
+			const user = await app
+			.auth()
+			.signInWithEmailAndPassword(email.value, password.value);
+			this.props.history.push("/");
+			console.log(user);
+		} catch (error) {
+			alert(error);
+		}
+	};
 
-  render() {
-    return <LogInView onSubmit={this.handleSignUp} />;
-  }
+	render() {
+		return <LogInView onSubmit={this.handleSignUp} />;
+	}
 }
 
 export default withRouter(LogInContainer);
