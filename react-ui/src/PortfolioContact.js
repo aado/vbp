38b3c27fd9ad1904def5a83a48717e390db2cda0
alvin <alvin@outsourceglobal.com>
2018-10-31 16:25:49 +0800

    changes

--- react-ui/src/PortfolioContact.js
+++ react-ui/src/PortfolioContact.js
@@ -5,7 +5,41 @@ const PortfolioContact = () => {
         <div className="container" style={{marginTop: '80px'}}>
             <h1>Contact</h1>
             <div className="row">
-                PortfolioContact
+            <section id="contact">
+                <div className="container">
+                    <div className="well well-sm">
+                        <h3><strong>Contact Us</strong></h3>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-7">
+                            Googlemap here
+                            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d3736489.7218514383!2d90.21589792292741!3d23.857125486636733!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1506502314230" width="100%" height="315" frameborder="0" style="border:0" allowfullscreen></iframe> */}
+                        </div>
+
+                        <div className="col-md-5">
+                            <h4><strong>Get in Touch</strong></h4>
+                            <form>
+                                <div className="form-group">
+                                    <input type="text" class="form-control" name="" value="" placeholder="Name" />
+                                </div>
+                                <div className="form-group">
+                                    <input type="email" class="form-control" name="" value="" placeholder="E-mail" />
+                                </div>
+                                <div className="form-group">
+                                    <input type="tel" class="form-control" name="" value="" placeholder="Phone" />
+                                </div>
+                                <div className="form-group">
+                                    <textarea className="form-control" name="" rows="3" placeholder="Message"></textarea>
+                                </div>
+                                <button className="btn btn-default" type="submit" name="button">
+                                    <i className="fa fa-paper-plane-o" aria-hidden="true"></i> Submit
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                </section>
             </div>
         </div>
     )
